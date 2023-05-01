ef8c4fdb5b41153827957d1c1b38e4d4072ba046
Sebastián Dávila <sebastian.davila@southerncode.us>
2023-05-01 07:02:16 -0300

    Hide sensitive information

--- catalog_app/catalog_app/settings.py
+++ catalog_app/catalog_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hhbnd-@bin41$+ok29_qt43v80ghi%+j+7zny6d@dj@5l&9ore'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
